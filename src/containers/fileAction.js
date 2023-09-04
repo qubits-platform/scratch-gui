4c73d8ce07130e910a08cdadafc27a88108a30e4
Akilkumar <akilkumar@fourbends.com>
2023-09-04 18:35:38 +0530

    remove unwanted space and set the session key from cookies

--- src/containers/fileAction.js
+++
@@ -1,8 +0,0 @@
-const FILE_UPLOAD = 'FILE_UPLOAD';
-
-
-export const fileUpload = (files) => ({
-    type: FILE_UPLOAD,
-    payload: files,
-  });
-
--- src/containers/setFunction.js
+++
@@ -1,7 +0,0 @@
-
-const SET_FUNCTION = 'SET_FUNCTION';
-
-export const setFunction = (func) => ({
-    type: SET_FUNCTION,
-    payload: func,
-  });
