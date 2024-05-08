44e03bf540140a9d20982e0e0fe4289d66ae160d
Raja Sekhar <rajasekhar@192.168.1.3>
2024-05-09 03:14:02 +0530

    fix:revert message changes

--- src/lib/shared-messages.js
+++ src/lib/shared-messages.js
@@ -32,7 +32,7 @@ export default defineMessages({
         description: 'Title for uploading a project from your computer'
     },
     loadFromLocalStorage: {
-        id: 'gui.sharedMessages.loadFromLocalStorage',
+        id: 'gui.sharedMessages.loadFromLocalStorages',
         defaultMessage: 'Load from your Local Storage',
         description: 'Title for uploading a project from your computer'
     },
